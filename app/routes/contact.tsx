1d1520fffeec129b061c0cd355f583b5a55dd3d3
nguyen9063nc-del <nguyen9063nc@gmail.com>
2026-08-18 18:24:55 -0700

    Revert the red frame around the Zoho form

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -20,9 +20,9 @@ export default function Contact() {
     <div>
       <Band tight style={{ paddingTop: 4 }}>
         <div style={{ display: "grid", gridTemplateColumns: "1.1fr .9fr", gap: 56, alignItems: "start" }}>
-          <Card tone="brand" padding="none" style={{ display: "flex", flexDirection: "column", padding: 8 }}>
+          <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
             <ZohoContractorForm />
-          </Card>
+          </div>
           <div style={{ display: "flex", flexDirection: "column", gap: 18, paddingTop: 8 }}>
             <p style={{ margin: 0, font: "var(--type-body)", color: "var(--text-body)", lineHeight: 1.6 }}>
               We will need some basic information from you so we can get on the same page. After you fill out the form we will contact you and we can discuss next steps and get the project started.
